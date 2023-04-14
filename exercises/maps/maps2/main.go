d6a20759a2f5028e83bd10748c2f53213c9eeea7
Alejo Rodriguez <atrodriguez@fi.uba.ar>
2023-04-13 21:18:58 -0300

    finished maps exercices

--- exercises/maps/maps2/main.go
+++ exercises/maps/maps2/main.go
@@ -1,12 +1,11 @@
 // maps2
 // Make me compile!
 //
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
-	m := map{}
+	m := map[string]int{"John": 20, "Ana": 25}
 	fmt.Printf("John is %d and Ana is %d", m["John"], m["Ana"])
 }
